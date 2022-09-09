86a494e2d93603070d239aa2e9f915c003f36062
Klous-jan <razer.nodder@gmail.com>
2022-09-08 22:29:46 -0700

    Updates and Adds
    Create Profile and Manage Profile pages are running and directing as needed. slider is still broken, working on that next.

---
+++ Notflicks-Netflix-Clone/JS/saveProfile.js
@@ -0,0 +1,132 @@
+class Profile {
+
+    setProfile(p, l, m, ne, pp) {
+        this.profileName = p;
+        this.language = l;
+        this.maturityRating = m;
+        this.ANE = ne;
+        this.APP = pp;
+
+        this.saveProfile();
+    }
+
+    getProfile() {
+        document.getElementById("profileID").value = localStorage.getItem('profileName');
+        document.getElementById("languageID").value = localStorage.getItem('languageName');
+        // document.getElementById("maturityRatingID").value = localStorage.getItem('maturityRatingName');
+        document.getElementById("ANEID").value = localStorage.getItem('ANEName');
+        document.getElementById("APID").value = localStorage.getItem('APName');
+    }
+
+    saveProfile() {
+        let storeProfile = document.getElementById('profileID').value;
+        localStorage.setItem('profileName', storeProfile);
+        let storeLanguage = document.getElementById('languageID').value;
+        localStorage.setItem('languageName', storeLanguage);
+        let storeMaturityRating = document.getElementById('maturityRatingID').value;
+        localStorage.setItem('maturityRatingName', storeMaturityRating);
+        let storeANE = document.getElementById('ANEID').value;
+        localStorage.setItem('ANEName', storeANE);
+        let storeAPP = document.getElementById('APID').value;
+        localStorage.setItem('APName', storeAPP);
+
+        alert('Data saved in localStorage succussfully!!')
+    }
+
+}
+
+function validateForm(p) {
+    p = p || window.event;
+    if (p.preventDefault) {
+        p.preventDefault();
+    } else {
+        p.returnValue = false;
+    }
+    // Profile Name
+    var xP = document.forms['Login']['profileName'].value;
+    if (xP == null || xP == "") {
+        alert("Please enter a profile name");
+        document.getElementById('profileID').focus();
+
+    } else if (xP.length < 3) {
+        alert("Profile name must be over 3 characters");
+        document.getElementById("profileID").focus();
+
+    }
+
+    // Language
+    var xL = document.getElementById("dropdown");
+
+    function onChange() {
+        var selected = xL.value;
+        var textRe = xL.options[xL.selectedIndex].value;
+        console.log(selected, textRe);
+    }
+    xL.onchange = onChange;
+    onChange();
+    console.log(onChange);
+
+    ////////////////////////////////////////////////
+    //// come back to this once slider is made. ////
+    ////////////////////////////////////////////////
+    ///////////////////////////   //////////////////-
+    //////////////////////////   ///////////////////
+    /////////////////////////   ////////////////////
+    ////////////////////////   /////////////////////
+    ////////////////                   /////////////
+    /////////////////                ///////////////
+    //////////////////             /////////////////
+    ///////////////////          ///////////////////
+    ////////////////////       /////////////////////
+    /////////////////////    ///////////////////////
+    ////////////////////// /////////////////////////
+    ////////////////////////////////////////////////
+
+    // Maturity Rating
+
+    // xMR = document.forms['Login']['ageField'].value;
+    // if (xMR == null || xMR == "") {
+    //     alert("age can not be empty");
+    //     document.getElementById('age').focus();
+
+    // } else if (parseInt(xMR) < 12 || parseInt(xMR) > 95) {
+    //     alert("age should be between 12 and 95");
+    //     document.getElementById('age').focus();
+
+    // }
+
+    // Autoplay Next Episode (ANE)
+    var checkedValANE = null;
+    var inEl = document.getElementsByClassName('ANECheck');
+    for (var i = 0; inEl[i]; ++i) {
+        if (inEl[i].checked) {
+            checkedValANE = inEl[i].value;
+            break;
+        }
+    }
+
+    // Autoplay Previews (AP)
+    var checkedValAP = null;
+    var inEl = document.getElementsByClassName('APCheck');
+    for (var i = 0; inEl[i]; ++i) {
+        if (inEl[i].checked) {
+            checkedValAP = inEl[i].value;
+            break;
+        }
+    }
+}
+
+function UpdateProfile() {
+    Profile = new Profile();
+    if (validateForm()) {
+        Profile.setProfile();
+    }
+    UpdateProfile()
+}
+console.log(UpdateProfile);
+
+function displayProfile() {
+    m = new Profile();
+    console.log(m)
+    m.getProfile();
+}
--- Notflicks-Netflix-Clone/Testing/JS/testing.js
+++ Notflicks-Netflix-Clone/Testing/JS/testing.js
@@ -1,26 +1,86 @@
-let x = Math.floor((Math.random() * 4) + 1);
-document.getElementById("demo").style.left = x;
+// let x = Math.floor((Math.random() * 4) + 1);
+// document.getElementById("demo").style.left = x;
 
-function test() {
-    if (x == 4) {
-        document.getElementById("demo").style.color = "blue";
-        document.getElementById("demo").innerHTML = x;
-    }
-}
+// function test() {
+//     if (x == 4) {
+//         document.getElementById("demo").style.color = "blue";
+//         document.getElementById("demo").innerHTML = x;
+//     }
+// }
 
-document.getElementById("demo").style.left = "10";
-document.getElementById("demo").innerHTML = x;
-if (x == 4) {
-    document.getElementById("demo").style.color = "blue";
-    document.getElementById("demo").innerHTML = x;
-} else if (x == 3) {
-    document.getElementById("demo").style.color = "green";
-    document.getElementById("demo").innerHTML = x;
-} else if (x == 2) {
-    document.getElementById("demo").style.color = "pink";
-    document.getElementById("demo").innerHTML = x;
-} else if (x == 1) {
-    document.getElementById("demo").style.color = "yellow";
-    document.getElementById("demo").innerHTML = x;
-}
-console.log(x)
+// document.getElementById("demo").style.left = "10";
+// document.getElementById("demo").innerHTML = x;
+// if (x == 4) {
+//     document.getElementById("demo").style.color = "blue";
+//     document.getElementById("demo").innerHTML = x;
+// } else if (x == 3) {
+//     document.getElementById("demo").style.color = "green";
+//     document.getElementById("demo").innerHTML = x;
+// } else if (x == 2) {
+//     document.getElementById("demo").style.color = "pink";
+//     document.getElementById("demo").innerHTML = x;
+// } else if (x == 1) {
+//     document.getElementById("demo").style.color = "yellow";
+//     document.getElementById("demo").innerHTML = x;
+// }
+// console.log(x)
+
+
+// let sid = document.getElementById("demo")
+// let s = document.defaultView.getComputedStyle("left: 10%");
+// if (s == "left: 10%") {
+//     document.getElementById("demo").innerHTML = "hello world"
+// }
+
+// function testfun() {
+//     var x = document.getElementsByTagName("span")[0];
+
+//     if (x.id === "demo") {
+//         x.style.fontSize = "100px";
+//     }
+// }
+
+// BACKUP FUNCTION
+// function testingpull() {
+//     var x = document.getElementsByTagName("span")[0];
+//     var s = document.defaultView.getComputedStyle("left: 10%");
+//     if (x.id == "demo" || s.style == "left: 10%") {
+//         x.innerHTML = "heya";
+//     }
+// }
+
+// function testingpull() {
+//     var x = document.defaultView.getComputedStyle("left: 10%");
+//     if (x.Style == "left: 10%") {
+//         document.getElementById("demo").innerHTML = "heyELLP";
+//     }
+// }
+
+// const matches = document.querySelectorAll("span.yes");
+// console.log(matches)
+
+// var selClass = document.getElementsByClassName("ui-slider-pip-1");
+
+// const first = document.getElementsByClassName("main");
+// console.log(first)
+// const second = first.getElementsByTagName("div");
+// console.log(second)
+
+// function clickOne() {
+
+//     return className.innerHTML = "Hello";
+// }
+// console.log(className);
+
+// var elements = document.getElementsByClassName('.ui-slider-pip-1');
+// var requiredElement = elements[0];
+// console.log(requiredElement)
+
+
+// PROGRESS
+// This function returns the full span class in consol.log
+// const matches = document.querySelector('.ui-slider-pip-1');
+// console.log(matches)
+
+// const matches = document.querySelector('.ui-slider-pip-1');
+// console.log(matches)
